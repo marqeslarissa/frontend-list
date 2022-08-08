89b469c4c617c790eebcc3c996a3479f3480d2d9
Larissa Marques <larissamarques@live.com.pt>
2022-08-08 12:53:57 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import ExpeditionPage from "./ExpeditionPage";
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <ExpeditionPage />;
+}
---
+++ src/ExpeditionPage.tsx
@@ -0,0 +1,10 @@
+import "./ExpeditionPage.scss";
+
+import { observer } from "mobx-react";
+import React from "react";
+
+function ExpeditionsPage(props: any): any {
+  return <>Hi</>;
+}
+
+export default observer(() => <ExpeditionsPage />);
